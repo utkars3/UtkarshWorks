be18642727f6c1b329d4ec30b98d169da74f2229
utkars3 <utkarshkesharwani3@gmail.com>
2025-12-10 11:14:25 +0530

    cloudinary issue fixed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const API_URL = 'https://utkarsh-works.vercel.app/api';
+// const API_URL = 'http://localhost:5000/api';
 
 const api = axios.create({
   baseURL: API_URL,
